098f4fac50b833b07209f370d90f6df96f2da5f3
Edson Pimenta <dev.edsonpimenta@gmail.com>
2025-06-18 14:46:05 -0300

    docs(api): adds @nestjs/swagger and configs it with basic info

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Conéctar - User Management API")
+    .setDescription("API for managing user accounts in Conéctar")
+    .setVersion("0.0.1")
+    .addTag("users")
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("api", app, documentFactory());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
