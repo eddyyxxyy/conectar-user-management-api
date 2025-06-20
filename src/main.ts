efb7958c4becb4d335b69e840573752dc997f76e
Edson Pimenta <dev.edsonpimenta@gmail.com>
2025-06-20 15:30:53 -0300

    feat(project): adds class-<validator/transformer> and global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from "@nestjs/core";
 import { ConfigService } from "@nestjs/config";
 import { AppModule } from "./app/app.module";
 import { setupSwagger } from "./config/setup-swagger";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   setupSwagger(app);
 
